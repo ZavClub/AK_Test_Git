02eb09a80ea29e03e226ce29ff2459f7b6e82b84
ZavClub <a.e.kulkov@gmail.com>
2023-02-16 10:12:03 +0300

    Создание блока поиска по задаче

--- FinalTest_1_022023.cs
+++ FinalTest_1_022023.cs
@@ -9,3 +9,28 @@ for (int i = 0; i < size; i++)
     string? username = Console.ReadLine();
     array[i] = username.ToString();
 }
+
+int tmp = 0;
+
+//  Блок поиска 3-х или нменее символов в введённых массивах и их вывод в консоль
+void FindLiterals()
+{
+
+    for (int index = 0; index < size; index++)
+    {
+        if (index == 0) Console.Write("[");
+
+        int num = 0;
+
+        foreach (char c in array[index]) num++;
+
+        if (num > 0 && num <= 3)
+        {
+            tmp++;
+            if (tmp > 1 && (num > 0 && num <= 3)) Console.Write(",");
+            Console.Write($"'{array[index]}'");
+        }
+        if (index == size - 1) Console.Write("]");
+    }
+}
+
